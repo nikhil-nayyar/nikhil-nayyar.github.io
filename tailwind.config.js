2a0efff099d68ecbb585330bdd1aa1cc6fc35ad1
nikhil nayyar <nikhil.sanjiv.nayyar@gmail.com>
2023-05-03 14:52:33 -0400

    update website colors with custom palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,6 +10,22 @@ module.exports = {
       serif: ['"Crimson Pro"', ...defaultTheme.fontFamily.serif],
       sans: ['"Work Sans"', ...defaultTheme.fontFamily.sans],
       mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
+    },
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
+      'forest': '#1a2716',
+      'ivy': '#a7b4a4',
+      'davy': '#5e5d5c',
+      'vanilla': '#f2dda4',
+      'mauve': '#2b061e'
     }
   },
   variants: {},
